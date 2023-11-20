60d6b620f08cb5fc22c5487c57f7beaefb06038f
Arpitha Nagappa <arpithagn021@gmail.com>
2023-11-20 15:30:03 -0600

    committing

--- part1/exercises/src/components/BookList.js
+++ part1/exercises/src/components/BookList.js
@@ -1,15 +1,15 @@
 export default function BookList() {
-   let pageTitle = "shrug";
-   let book1 = "google.com";
-   let book2 = "";
-   let book3 = "";
+   let pageTitle = "Latest Book Releases";
+   let book1 = "https://m.media-amazon.com/images/I/61O01hAKJ3L._AC_UF1000,1000_QL80_.jpg";
+   let book2 = "https://photos-us.bazaarvoice.com/photo/2/cGhvdG86YmFybmVzYW5kbm9ibGU/b5054cfc-589a-51e7-94d8-979fcd68f434";
+   let book3 = "https://images.squarespace-cdn.com/content/v1/5e720e1b43b5943ba1d32682/1680795106276-0ML2RYQZWHLK5HN1FQ6X/IMG_2454.jpg?format=1000w";
 
    return (
       <div>
          <h3>{pageTitle}</h3>
-         <img src={book1} alt="Nothing here!" />
-         <img src={book2} alt="Nothing here!" />
-         <img src={book3} alt="Nothing here!" />
+         <img src={book1} alt="Wings of Fire!" />
+         <img src={book2} alt="The London Seance Society by Sarah Penner!" />
+         <img src={book3} alt="Romantic Comedy by Curtis Sittenfield!" />
       </div>      
    );
 }
--- part1/exercises/src/components/ChoresList.js
+++ part1/exercises/src/components/ChoresList.js
@@ -1,3 +1,15 @@
+import React from 'react';
+import './ChoresList.module.css';
+
 export default function ChoresList () {
-   return <p>No content is here!</p>;
+   return (
+   <div>
+   <h3>Today's Chore</h3>
+   <ol>
+      <li>Empty Dishwaswer</li>
+      <li>Complete prepWork</li>
+      <li>Buy groceries</li>
+   </ol>
+</div>
+   );
 }
